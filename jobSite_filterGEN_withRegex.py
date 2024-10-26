7ab7cb171fad6a0c7b5fa1bd5a7e9e7d90e124c3
James.R <hilarionjames25@gmail.com>
2024-10-25 20:34:29 -0400

    filter out 'applied'

--- jobSite_filterGEN_withRegex.py
+++ jobSite_filterGEN_withRegex.py
@@ -77,7 +77,8 @@ def create_company_filter_list():
             'linkedin.com##li.ember-view.jobs-search-results__list-item:has(span.job-card-container__primary-description:has-text(/({pattern})/i))',
             "",
             "! Linkedin Filter-Out: Removed from job feed (Removed, Applied)",
-            'linkedin.com##li.ember-view.jobs-search-results__list-item:has(div.artdeco-entity-lockup__caption:has(span:has-text(We won’t show you this job again)))'
+            'linkedin.com##li.ember-view.jobs-search-results__list-item:has(div.artdeco-entity-lockup__caption:has(span:has-text(We won’t show you this job again)))',
+            'linkedin.com##li.ember-view.jobs-search-results__list-item:has(ul.job-card-list__footer-wrapper:has(li.job-card-container__footer-job-state:has-text(Applied)))'
         ],
         "Indeed": [
             "! Indeed Job Search Feed",
