621a966d4160ff1d2c2616522d1c9a74d49659bd
James.R <hilarionjames25@gmail.com>
2024-11-25 15:14:29 -0500

    update sample + filter + program

--- jobSite_filterGEN_withRegex.py
+++ jobSite_filterGEN_withRegex.py
@@ -74,11 +74,11 @@ def create_company_filter_list():
             'linkedin.com##li.reusable-search__result-container:has(div.entity-result__primary-subtitle:has-text(/({pattern})/i))',
             "",
             "! Linkedin Job Feed - updated",
-            'linkedin.com##li.ember-view.jobs-search-results__list-item:has(span.cSgAFzYHhlVAudlPUBRKZppIxOTCPQFaRzd:has-text(/({pattern})/i))',
+            'linkedin.com##li.ember-view:has(div.artdeco-entity-lockup__subtitle:has-text(/({pattern})/i))',
             "",
             "! Linkedin Filter-Out: Removed from job feed (Removed, Applied)",
             'linkedin.com##li.ember-view.jobs-search-results__list-item:has(div.artdeco-entity-lockup__caption:has(span:has-text(We won’t show you this job again)))',
-            'linkedin.com##li.ember-view.jobs-search-results__list-item:has(div.job-card-container__footer-item--highlighted:has-text(We won’t show you this job again))',
+            'linkedin.com##li.ember-view:has(div.job-card-container__footer-item--highlighted:has-text(We won’t show you this job again))',
             '! linkedin.com##li.ember-view.jobs-search-results__list-item:has(ul.job-card-list__footer-wrapper:has(li.job-card-container__footer-job-state:has-text(Applied)))'
         ],
         "Indeed": [
